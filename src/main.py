aeaf6718722fb0704be49682fde231820ce61c8a
Matija Martinic <crazy4magic@gmail.com>
2014-03-15 18:53:47 +0100

    Comment.

--- src/main.py
+++ src/main.py
@@ -2,7 +2,9 @@ from core.downloader import downloader
 import core.fedora_bugzilla as fedora
 """
 Naravno nece biti ovako 'smotano', nego sada cisto za prvu ruku
-da vidite dali je to dobro
+da vidite dali je to dobro. Ako budete pokretali, morate znati
+da je njihova stranica uzasno spora jer treba svih 1000 redaka
+ispisati.
 """
 def main():
 	test_url = "https://bugzilla.redhat.com/buglist.cgi?bug_status=__open__&"\
