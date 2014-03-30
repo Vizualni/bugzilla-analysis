196e18b3996a94831067b7d764bcebfd30bd5b18
Matija Martinic <crazy4magic@gmail.com>
2014-03-30 15:37:50 +0200

    Remove deleted files

--- src/core/entity.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-class Entity:
-	"""This is one entity from database.
-	It is going to access for data like this:
-	entity1 = extractor(...).getData(...)[0]
-	print entity1.ID, entity1.bugs, entity1.software_name
-	while bugs is also going to be entity...
-	"""
